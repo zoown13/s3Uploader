746633d8c6d636244ea21487dae9a7c51a7c579c
EC2 Default User <ec2-user@ip-172-31-31-160.ap-northeast-2.compute.internal>
2021-05-31 12:30:28 +0000

    return domain removed url

--- app.py
+++ app.py
@@ -10,26 +10,8 @@ def uploads(name):
     url = client.generate_presigned_url('put_object', Params= {'Bucket': "media-query-mediabucket-1i4slys4cekco",
                                                         "Key": name, "ContentType": 'image/jpeg'},
                                                          ExpiresIn=300)
+    if name in url:
+        index = url.find(name)
+        url = url[index-1:]
                                                     
-    return {'uploadUrl': url}
-
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+    return url                                                
